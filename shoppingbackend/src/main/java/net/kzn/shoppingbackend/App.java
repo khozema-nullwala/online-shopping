59b414ea923c3baa7d9b9131b5572edc4b6c93d8
Khozema Nullwala <khozema.nullwala@gmail.com>
2017-02-10 10:17:40 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kzn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
